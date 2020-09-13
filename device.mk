51475e353bacf7e2d69d22156ec07933a21c29ba
improbir <ghosh_probir@yahoo.com>
2020-09-13 04:30:42 +0000

    RMX1921: Build ANT
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService
+
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
